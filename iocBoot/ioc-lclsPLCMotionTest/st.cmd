72a58591ab9f66f7a666d1a931de76aaaa0bb5d1
Yann Stephen Mandza <ysmandza@slac.stanford.edu>
2025-09-30 16:16:57 -0700

    Using LCLS TwinCAT Motion Library

--- iocBoot/ioc-lclsPLCMotionTest/st.cmd
+++ iocBoot/ioc-lclsPLCMotionTest/st.cmd
@@ -1,9 +1,9 @@
-#!/opt/epics/iocs/ads-ioc/R1.0.0/bin/rhel9-x86_64/adsIoc
+#!c:/Repos/ads-ioc/R0.8.0///bin/rhel7-x86_64/adsIoc
 ################### AUTO-GENERATED DO NOT EDIT ###################
 #
 #         Project: lcls-motion-test.tsproj
 #        PLC name: lclsPLCMotionTest (lclsPLCMotionTest Instance)
-# Generated using: pytmc 2.18.3.dev2+g94c62dbb0
+# Generated using: pytmc 2.18.2
 # Project version: unknown
 #    Project hash: unknown
 #     PLC IP/host: 
@@ -15,7 +15,7 @@
 # Libraries:
 #
 #   LCLS General: * (SLAC)
-#   LCLS_OMotion: * (SLAC)
+#   lcls-twincat-motion-abstraction: * (SLAC)
 #   Tc2_MC2: * (Beckhoff Automation GmbH)
 #   Tc2_NC: * (Beckhoff Automation GmbH)
 #   Tc2_Standard: * -> 3.4.5.0 (Beckhoff Automation GmbH)
@@ -23,13 +23,11 @@
 #   Tc3_Module: * (Beckhoff Automation GmbH)
 #
 ################### AUTO-GENERATED DO NOT EDIT ###################
-# Run common startup commands for linux soft IOC's
-< $(IOC_COMMON)/All/pre_linux.cmd
 < envPaths
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "epics-dev" )
+epicsEnvSet("ENGINEER", "" )
 epicsEnvSet("LOCATION", "PLC:lclsPLCMotionTest" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
@@ -42,7 +40,7 @@ epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
 epicsEnvSet("IPADDR",           "")
 epicsEnvSet("AMSID",            "")
 epicsEnvSet("AMS_PORT",         "851")
-epicsEnvSet("ADS_MAX_PARAMS",   "1006")
+epicsEnvSet("ADS_MAX_PARAMS",   "1070")
 epicsEnvSet("ADS_SAMPLE_MS",    "50")
 epicsEnvSet("ADS_MAX_DELAY_MS", "100")
 epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
@@ -79,56 +77,6 @@ adsAsynPortDriverConfigure("$(ASYN_PORT)", "$(IPADDR)", "$(AMSID)", "$(AMS_PORT)
 cd "$(ADS_IOC_TOP)/db"
 
 
-epicsEnvSet("MOTOR_PORT",     "PLC_ADS")
-epicsEnvSet("PREFIX",         "PLC:lclsPLCMotionTest:")
-epicsEnvSet("NUMAXES",        "2")
-epicsEnvSet("MOVE_POLL_RATE", "200")
-epicsEnvSet("IDLE_POLL_RATE", "1000")
-
-EthercatMCCreateController("$(MOTOR_PORT)", "$(ASYN_PORT)", "$(NUMAXES)", "$(MOVE_POLL_RATE)", "$(IDLE_POLL_RATE)")
-
-#define ASYN_TRACE_ERROR     0x0001
-#define ASYN_TRACEIO_DEVICE  0x0002
-#define ASYN_TRACEIO_FILTER  0x0004
-#define ASYN_TRACEIO_DRIVER  0x0008
-#define ASYN_TRACE_FLOW      0x0010
-#define ASYN_TRACE_WARNING   0x0020
-#define ASYN_TRACE_INFO      0x0040
-asynSetTraceMask("$(ASYN_PORT)", -1, 0x41)
-
-#define ASYN_TRACEIO_NODATA 0x0000
-#define ASYN_TRACEIO_ASCII  0x0001
-#define ASYN_TRACEIO_ESCAPE 0x0002
-#define ASYN_TRACEIO_HEX    0x0004
-asynSetTraceIOMask("$(ASYN_PORT)", -1, 2)
-
-#define ASYN_TRACEINFO_TIME 0x0001
-#define ASYN_TRACEINFO_PORT 0x0002
-#define ASYN_TRACEINFO_SOURCE 0x0004
-#define ASYN_TRACEINFO_THREAD 0x0008
-asynSetTraceInfoMask("$(ASYN_PORT)", -1, 5)
-
-#define AMPLIFIER_ON_FLAG_CREATE_AXIS  1
-#define AMPLIFIER_ON_FLAG_WHEN_HOMING  2
-#define AMPLIFIER_ON_FLAG_USING_CNEN   4
-
-epicsEnvSet("AXIS_NO",         "1")
-epicsEnvSet("MOTOR_PREFIX",    "TST:MOTION:")
-epicsEnvSet("MOTOR_NAME",      "M1")
-epicsEnvSet("MOTOR_ADS_PATH",  "MAIN.fbMotionStage")
-epicsEnvSet("DESC",            "MAIN.fbMotionStage / Axis 1")
-epicsEnvSet("EGU",             "mm")
-epicsEnvSet("PREC",            "3")
-epicsEnvSet("AXISCONFIG",      "")
-epicsEnvSet("ECAXISFIELDINIT", "")
-epicsEnvSet("AMPLIFIER_FLAGS", "")
-
-EthercatMCCreateAxis("$(MOTOR_PORT)", "$(AXIS_NO)", "$(AMPLIFIER_FLAGS)", "$(AXISCONFIG)")
-dbLoadRecords("EthercatMC.template", "PREFIX=$(MOTOR_PREFIX), MOTOR_NAME=$(MOTOR_NAME), R=$(MOTOR_NAME)-, MOTOR_PORT=$(MOTOR_PORT), ASYN_PORT=$(ASYN_PORT), AXIS_NO=$(AXIS_NO), DESC=$(DESC), PREC=$(PREC), EGU=$(EGU) $(ECAXISFIELDINIT)")
-dbLoadRecords("EthercatMCreadback.template", "PREFIX=$(MOTOR_PREFIX), MOTOR_NAME=$(MOTOR_NAME), R=$(MOTOR_NAME)-, MOTOR_PORT=$(MOTOR_PORT), ASYN_PORT=$(ASYN_PORT), AXIS_NO=$(AXIS_NO), DESC=$(DESC), PREC=$(PREC) ")
-dbLoadRecords("EthercatMCdebug.template", "PREFIX=$(MOTOR_PREFIX), MOTOR_NAME=$(MOTOR_NAME), MOTOR_PORT=$(MOTOR_PORT), AXIS_NO=$(AXIS_NO), PREC=3")
-
-
 dbLoadRecords("iocSoft.db", "IOC=PLC:lclsPLCMotionTest")
 dbLoadRecords("save_restoreStatus.db", "P=PLC:lclsPLCMotionTest:")
 dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
@@ -137,16 +85,12 @@ dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:lclsPLCMotionTest,IDX=1,TASK_PORT=350")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:lclsPLCMotionTest")
 
-<<<<<<< HEAD
 dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:lclsPLCMotionTest,PROJECT=lcls-motion-test.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.18.2,PLC_HOST=")
-=======
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:lclsPLCMotionTest,PROJECT=lcls-motion-test.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.18.3.dev2+g94c62dbb0,PLC_HOST=192.168.206.130")
->>>>>>> aac9545000f9286eb341bd720a5f209b5a624609
 
 #   LCLS General: * (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:lclsPLCMotionTest,DEPENDENCY=LCLS_General,VERSION=*,VENDOR=SLAC")
-#   LCLS_OMotion: * (SLAC)
-dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:lclsPLCMotionTest,DEPENDENCY=LCLS_OMotion,VERSION=*,VENDOR=SLAC")
+#   lcls-twincat-motion-abstraction: * (SLAC)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:lclsPLCMotionTest,DEPENDENCY=lcls-twincat-motion-abstraction,VERSION=*,VENDOR=SLAC")
 #   Tc2_MC2: * (Beckhoff Automation GmbH)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:lclsPLCMotionTest,DEPENDENCY=Tc2_MC2,VERSION=*,VENDOR=Beckhoff Automation GmbH")
 #   Tc2_NC: * (Beckhoff Automation GmbH)
@@ -163,8 +107,8 @@ cd "$(IOC_TOP)"
 ## PLC Project Database files ##
 dbLoadRecords("lclsPLCMotionTest.db", "PORT=$(ASYN_PORT),PREFIX=PLC:lclsPLCMotionTest:,IOCNAME=$(IOC),IOC=$(IOC)")
 
-# Total records: 6
-callbackSetQueueSize(2012)
+# Total records: 70
+callbackSetQueueSize(2140)
 
 # Autosave and archive settings:
 save_restoreSet_status_prefix("PLC:lclsPLCMotionTest:")
